fed4a9d2d58213e045dcbcfe1c4f57cab77513f7
quoc thai <lqthai5121@gmail.com>
2023-07-05 17:21:54 +0700

    add size

--- app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/client.tsx
@@ -1,21 +1,22 @@
-"use client"
+'use client'
 
 import { useParams, useRouter } from "next/navigation"
 import { Plus } from "lucide-react"
 
-import { ApiList } from "@/components/ui/api-list"
-import { DataTable } from "@/components/ui/data-table"
+
 import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
-import { CategoryColumn, columns } from "./columns"
+import { SizeColumn, columns } from "./columns"
+import { DataTable } from "@/components/ui/data-table"
+import { ApiList } from "@/components/ui/api-list"
 
 
-interface CategoryClientProps {
-    data: CategoryColumn[]
+interface SizesClientProps {
+    data: SizeColumn[]
 }
 
-export const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
+export const SizesClient: React.FC<SizesClientProps> = ({ data }) => {
     const router = useRouter()
     const params = useParams()
 
@@ -23,19 +24,19 @@ export const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
         <>
             <div className="flex items-center justify-between">
                 <Heading
-                    title={`Categories (${data.length})`}
-                    description="Manage categories for your store"
+                    title={`Sizes (${data.length})`}
+                    description="Manage sizes for your store"
                 />
-                <Button onClick={() => router.push(`/${params.storeId}/categories/new`)}>
+                <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
                     <Plus className="mr-2 h-4 w-4" />
                     Add New
                 </Button>
             </div>
             <Separator />
             <DataTable columns={columns} data={data} searchKey="name" />
-            <Heading title="API" description="API call for Categories" />
+            <Heading title="API" description="API call for Sizes" />
             <Separator />
-            <ApiList entityName="categories" entityIdName="categoryId" />
+            <ApiList entityName="sizes" entityIdName="sizeId" />
         </>
     )
 }
